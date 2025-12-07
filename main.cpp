303fb1582237bcf078723c63b73c36012847063b
Amir Mohammad Mohammadi <amirmohammad.gharebagh1385@gmail.com>
2025-12-08 01:18:52 +0330

    Qarebaq - main cpp file created with a little test case for testing entities of this game

---
+++ main.cpp
@@ -0,0 +1,12 @@
+
+
+#include <iostream>
+using namespace std;
+
+#include "minion.h"
+
+int main() {
+    Minion m("Alleycat", 1, 1, 1); //testCase that shows everything is ok now
+    m.play();
+    return 0;
+}
