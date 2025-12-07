c2a8514200ce177a59b004d9080fecedf58a4f73
Amir Mohammad Mohammadi <amirmohammad.gharebagh1385@gmail.com>
2025-12-08 01:17:27 +0330

    Qarebaq - a little bug and syntax error fixed

---
+++ minion.h
@@ -0,0 +1,31 @@
+#ifndef MINION_H
+#define MINION_H
+
+#include "Card.h"
+#include <iostream>
+using namespace std;
+
+class Minion : public Card {
+public:
+    int attack;
+    int health;
+    bool taunt;
+    bool divineShield;
+    bool deathrattle;
+
+    Minion(string n, int t, int atk, int hp)
+        : Card(n, t, CardType::Minion),
+          attack(atk),
+          health(hp),
+          taunt(false),
+          divineShield(false),
+          deathrattle(false)
+    {}
+
+    void play() override {
+        cout << name << " entered the board!" << endl;
+    }
+};
+
+
+#endif
