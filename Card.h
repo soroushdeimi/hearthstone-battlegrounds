7ec7fafe5e073bdfe43247c4167be545728c4b46
Amir Mohammad Mohammadi <amirmohammad.gharebagh1385@gmail.com>
2025-12-08 00:55:37 +0330

    Qarebaq - Header file for Card class created

---
+++ Card.h
@@ -0,0 +1,38 @@
+#ifndef CARD_H
+#define CARD_H
+
+
+#include <string>
+#include<iostream>
+
+using namespace std;
+
+
+enum CardType{
+Hero,
+Minion
+
+};
+
+class Card{
+
+    public:
+        CardType type;
+        string name;
+        int tier;
+        // int cost;   in battleground the cost of each minion is always 3 coins 
+        string description;
+
+        //constructors are here
+        Card():type(CardType::Minion),name(""),tier(1),description(""){}
+        Card(string n, int t, CardType tp): name(n), tier(t), type(tp) {}
+
+
+
+        virtual void play(){}; //override in child classes
+        virtual ~Card() {} 
+};
+
+
+
+#endif
