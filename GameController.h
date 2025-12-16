e3a5d8c5fc9c380434a27ece02fd913ab43fccd7
Amir Mohammad Mohammadi <amirmohammad.gharebagh1385@gmail.com>
2025-12-17 03:08:33 +0330

    Qarebaq - GameController aded and created(دارم هنوز زور میزنم مدل رو از کنترلر جدا کنم)

---
+++ GameController.h
@@ -0,0 +1,15 @@
+#ifndef GAME_CONTROLLER_H
+#define GAME_CONTROLLER_H
+#include "Shop.h"
+#include "Player.h"
+
+
+
+class GameController{
+    public:
+    void buyPhase(Player &p, Shop &shop);
+
+};
+
+
+#endif
