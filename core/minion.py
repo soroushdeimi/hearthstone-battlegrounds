7c5bc3b0e44d2babad271acefea32ba83a86f733
Manishahsavari <shahsavari.mani@gmail.com>
2025-12-18 21:20:03 +0330

    Shahsavari - Define class of Minions with parameters name , attack , health , tier , and write manual test for check class

---
+++ core/minion.py
@@ -0,0 +1,22 @@
+class Minions:
+    def __init__(self, name,  attack , health , tier):
+        self.name = name
+        self.attack = attack
+        self.health = health
+        self.tier = tier
+
+
+    def is_alive(self):
+        return self.health > 0
+
+    def take_damage(self , dmg):
+        self.health -= dmg
+
+minion2 = Minions("Ali", 5,2,1);
+
+minion2.take_damage(3)
+if not minion2.is_alive():
+    print("Dead")
+else:
+    print("alive")
+
