a1baca35465a1c12b9d1a2906cae0cedf3594b4a
Manishahsavari <shahsavari.mani@gmail.com>
2025-12-19 01:02:00 +0330

    Shahsavari - Wrote a class for minions.py and players.py and defined arbitrary for each file:)

--- core/engine/minion.py
+++ core/engine/minion.py
@@ -1,24 +1,22 @@
 import pygame
-import minion
 class Minion:
     def __init__(self, name,  attack , health , tier):
         self.name = name
         self.attack = attack
         self.health = health
         self.tier = tier
-
-
-    def is_alive(self):
-        return self.health > 0
-
+        self.is_alive = True;
     def take_damage(self , dmg):
         self.health -= dmg
-
-minion2 = Minions("Ali", 5,2,1);
-
-minion2.take_damage(3)
-if not minion2.is_alive():
-    print("Dead")
-else:
-    print("alive")
+        if self.health <= 0:
+            self.health = 0
+            self.is_alive = False
+#
+# minion2 = Minion("Ali", 5,2,1);
+#
+# minion2.take_damage(3)
+# if not minion2.is_alive():
+#     print("Dead")
+# else:
+#     print("alive")
 
--- core/engine/player.py
+++ core/engine/player.py
@@ -1,15 +1,33 @@
 class Player:
-    def __init__(self , hp , gold , board):
-        hp = 30
-        gold = 0
-        board  = []
+    def __init__(self, name):
+        self.name = name
+        self.hp = 30
+        self.gold = 0
+        self.board  = []
+        self.tavern_tier = 1
+        self.shop = []
 
-    def add_minion():
-        if(i <= 7):
-            self.board.extend();
+    def add_minion(self , minion):
+        if len(self.board) < 7:
+            self.board.append(minion)
         else:
-            print("unvalid")
-    def remove_minion(n):
-            self.board.remove(n)
-    def take_damage(n)
+            print("Board Full!!!!")
+    def remove_minion(self , n):
+        if 0 <= n < len(self.board):
+           return self.board.pop(n)
+        return None
+    def take_damage(self , amount):
+            self.hp -= amount
+    def rest_gold(self):
+        self.gold = 0
+    def roll_shop(self, tavern):
+        pass
+    def buy(self ,shop_index):
+        pass
+    def upgrade_tier(self):
+        self.tavern_tier += 1
+    def is_alive(self):
+        return self.hp > 0
+
+
 
