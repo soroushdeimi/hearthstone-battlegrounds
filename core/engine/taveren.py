bc9bfeba8a35c822c861860c3c1bb5fc3b9fcd01
Manishahsavari <shahsavari.mani@gmail.com>
2025-12-19 15:27:03 +0330

    Shahsavari - Wrote a class for taverns

---
+++ core/engine/taveren.py
@@ -0,0 +1,19 @@
+import random
+from minion import Minion
+
+class Tavern:
+    def __init__(self , shop_size = 3):
+        self.shop_size = shop_size
+        self.minion_pool = self._create_minion_pool()
+
+    def _create_minion_pool(self):
+        return [
+            Minion("Catacomb Crasher" , 1 ,1,)
+        ]
+    def get_available_minions(self, tier):
+        return [m for m in self.minion_pool if m.tier <= tier]
+    def roll(self, player_tier):
+        available = self.get_available_minions(player_tier)
+        if not available:
+            return []
+        return random.choices(available, k = self.shop_size)
