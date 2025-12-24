28c1f7fa03029d87d9c035291279cafbb7fab5f5
Parnian <parinianasghari84@gmail.com>
2025-12-24 15:04:04 +0330

    Add HeroSelect, Gameplay screens and hero transfer logic

--- src/gamee.py
+++ src/gamee.py
@@ -1,27 +1,35 @@
 import pygame
 from src.title_screen import Title
+from src.hero_select import HeroSelect  
+from src.gameplay import Gameplay
+
 class Game:
     def __init__(self):
         pygame.init()
         pygame.font.init()
+
         self.screens = {}
         self.current_screen = None
-
         self.screen = pygame.display.set_mode((900, 600))
         self.clock = pygame.time.Clock()
         self.f = 60
-        self.r = True # game is runing
+        self.r = True  # game is running
+
+        self.hero_selected = None  # برای ذخیره هیرو انتخاب شده
 
         self.screens["title"] = Title(self.screen, self.change_scr)
+        self.screens["hero_select"] = HeroSelect(self.screen, self.change_scr)
+
         self.change_scr("title")
 
     def run(self):
-        while(self.r):
+        while self.r:
             self.clock.tick(self.f)
             events = pygame.event.get()
             for event in events:
                 if event.type == pygame.QUIT:
                     self.r = False
+
             if self.current_screen is not None:
                 self.current_screen.handle_events(events)
                 self.current_screen.updates()
@@ -32,7 +40,10 @@ class Game:
             pygame.display.flip()
         pygame.quit()
 
-    def change_scr(self, name_scr):
+    def change_scr(self, name_scr, **kwargs):
+        
+        if name_scr == "gameplay" and "hero" in kwargs:
+            self.hero_selected = kwargs["hero"]
+            self.screens["gameplay"] = Gameplay(self.screen, self.change_scr, self.hero_selected)
+
         self.current_screen = self.screens[name_scr]
-
-
