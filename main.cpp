44c6eb714f6e079e7c3c6c05cfda98fe15c9f335
Amir Mohammad Mohammadi <amirmohammad.gharebagh1385@gmail.com>
2026-01-02 17:41:53 +0330

    Qarebaq -

--- main.cpp
+++ main.cpp
@@ -34,25 +34,23 @@
 
 int main(){// اینجا اومدیم تست درست کردیم
     
-    // ساختِ state (ممکنه ازت بخواد هیرو انتخاب کنی؛ این مرحله interactive است)
+    
     GameState state;
 
-    // ======= تست خودکار: اضافه کردن اکشن‌ها برای player0 =========
-    // player0: Roll, Buy slot 0, EndTurn
+   
     Action a;
     a.type = ActionType::Roll;
     state.pendingActions[0].push_back(a);
 
     Action b;
     b.type = ActionType::Buy;
-    b.slotIndex = 0; // خرید از اسلات 0 (اگر خالی یا نامعتبر بود، shop.buy آن را رد خواهد کرد)
+    b.slotIndex = 0; 
     state.pendingActions[0].push_back(b);
 
     Action e;
     e.type = ActionType::EndTurn;
     state.pendingActions[0].push_back(e);
 
-    // برای بقیه بازیکنان هم EndTurn بفرست تا سریع عبور کنند
     for(int i = 1; i < (int)state.players.size(); ++i){
         Action end;
         end.type = ActionType::EndTurn;
